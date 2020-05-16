b243030d1c61d8f3a61787d33b138701f0aa0fec
sai <saithechinnu@gmail.com>
2020-05-16 13:42:13 +0530

    react v4

--- src/components/Analyse.js
+++ src/components/Analyse.js
@@ -1,10 +1,20 @@
 import React,{Component} from 'react';
 import Loader from './Loader';
 import Aspiterator from './Aspects/Aspiterator';
+import {Link} from 'react-router-dom';
+import SvgIcon from '@material-ui/core/SvgIcon';
+import {Menu,MenuItem} from '@material-ui/core';
 
-
+function HomeIcon (props) {
+    return (
+      <SvgIcon {...props}>
+        <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+      </SvgIcon>
+    );
+  }
 export class Analyse extends Component {
   
+    
 
     render(){
         if(this.props.loading){
@@ -19,9 +29,13 @@ export class Analyse extends Component {
         }
     return (
         <div>
-            <Link ></Link>
-            <h1>Analyse</h1>
-            <Aspiterator data = {this.props.reviews} ></Aspiterator>
+           <div style ={{marginLeft:100}}>
+            <Link to="/" style={{textDecoration: 'none'}}>
+                <HomeIcon style={{fontSize: 50}} color="primary"/>
+            </Link>
+        </div>
+        <h1>Analysis</h1>
+        <Aspiterator data = {this.props.reviews} ></Aspiterator>
         </div>
     )
     }
